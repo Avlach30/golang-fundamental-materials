71bad6756ba16aa3bdc357e5ba679dc0ee765f3c
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-29 12:16:15 +0800

    convert number data type to another number

--- main.go
+++ main.go
@@ -9,4 +9,9 @@ func main() {
 	lStr := string(helloStr[2]) //* Get single character from existing variable of string
 	fmt.Println(lStr)
 	println("========")
+	//* Converting number to another number
+	scores := 247
+	fmt.Println(scores)
+	averageScore := float32(scores) / 3
+	fmt.Println(averageScore)
 }
