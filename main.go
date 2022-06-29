3d2e24bc882410d09ec4167f60f5fa99556d6211
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-29 10:49:48 +0800

    define number data type

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World");
+	//* Define number data type, integer and float
+	fmt.Println("Satu:", 1)
+	fmt.Println("Negatif tiga:", -3)
+	fmt.Println("Tiga koma lima:", 3.5)
 }
