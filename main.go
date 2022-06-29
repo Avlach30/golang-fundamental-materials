f330728f14d3b7d07092d5e715108dd620e63120
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-29 11:17:08 +0800

    declarating empty variable with assign value indirectly

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World");
+	var name string //* Declarating empty variable with string data type
+
+	name = "Ahmad Muzakki" //* Reassign existing variable
+	fmt.Println(name)
+	println("========")
+	name = "Ahmad Sujancok"
+	fmt.Println(name)
 }
