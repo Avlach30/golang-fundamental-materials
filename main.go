fba470118eaaed6a1ab54a0bb34dde9b472d55b7
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-29 13:00:38 +0800

    define math operations in go

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World");
+	firstNum := 10
+	secondNum := 18
+
+	//* Define math operations
+	fmt.Println(firstNum + secondNum)
+	fmt.Println(secondNum % firstNum)
+	fmt.Println(-10) //* Define negative number value
 }
