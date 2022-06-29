75b84dd7b0a62ee922d9fa2d840d07f51800d7ca
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-29 10:58:33 +0800

    define boolean data type

--- main.go
+++ main.go
@@ -7,4 +7,8 @@ func main() {
 	fmt.Println("Satu:", 1)
 	fmt.Println("Negatif tiga:", -3)
 	fmt.Println("Tiga koma lima:", 3.5)
+	println("======")
+	//* Define boolean data type, true or false
+	fmt.Println("True: ", true)
+	fmt.Println("False: ", false)
 }
