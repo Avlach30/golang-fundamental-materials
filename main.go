aae1dd287375e99e51449a5d8bcbae69250875ce
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-29 12:11:21 +0800

    convert string data type to another string

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World");
+	//* Converting string to another string
+	helloStr := "Hello"
+	fmt.Println(helloStr)
+	lStr := string(helloStr[2]) //* Get single character from existing variable of string
+	fmt.Println(lStr)
+	println("========")
 }
